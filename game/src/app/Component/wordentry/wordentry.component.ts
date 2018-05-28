8a874d0fb36d21e8b818d77cb4a89a30c82cc395
jimmy-ye3 <jhy2116@columbia.edu>
2018-05-28 10:35:05 -0400

    Client side can show the different images of game state

--- game/src/app/Component/wordentry/wordentry.component.ts
+++ game/src/app/Component/wordentry/wordentry.component.ts
@@ -7,13 +7,47 @@ import {WordentryService} from '../../services/wordentry.service'
 })
 
 export class WordentryComponent{
-    title: string;
+    word: string;
+    imagePath: string;
+    state: number;
     constructor(private entryword: WordentryService){
-       
+       this.imagePath = "https://image.ibb.co/f0zDVy/State0.png";
+       this.state=0;
     }
+
+    
 
     addTask(event){
         event.preventDefault();
-        console.log(this.title);
+        
+        var len=this.word.length;
+        if(len==1){
+            console.log("You entered a single character!");
+        }
+        else if (len==8){
+            console.log('You entered a word!');
+        }
+        else{
+            console.log('invalid entry');
+        }
+        
+        //change picture
+        this.state=this.state+1;
+
+        switch(this.state) {
+            case 0: this.imagePath="https://image.ibb.co/f0zDVy/State0.png"; break;
+            case 1: this.imagePath="https://image.ibb.co/fwxdxd/State1.png"; break;
+            case 2: this.imagePath="https://image.ibb.co/cnGriJ/State2.png"; break;
+            case 3: this.imagePath="https://image.ibb.co/dcZQcd/State3.png"; break;
+            case 4: this.imagePath="https://image.ibb.co/bCvWHd/State4.png"; break;
+            case 5: this.imagePath="https://image.ibb.co/g6f3Vy/State5.png"; break;
+            case 6: this.imagePath="https://image.ibb.co/hzuVAy/State6.png"; break;
+            case 7: this.imagePath="https://image.ibb.co/cGjmiJ/State7.png"; break;
+            case 8: this.imagePath="https://image.ibb.co/gc76iJ/State8.png"; break;
+            case 9: this.imagePath="https://image.ibb.co/mYuK3J/State9.png"; break;
+            case 10: {this.imagePath="https://image.ibb.co/k9o1Hd/State10.png"; this.state =-1; break;}
+        }
+        
+        
     }
 }
