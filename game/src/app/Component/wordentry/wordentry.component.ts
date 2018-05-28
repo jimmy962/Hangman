fb222e45be33f03b1a8b31430ef0189a598960a6
jimmy-ye3 <jhy2116@columbia.edu>
2018-05-28 11:47:47 -0400

    More client side stuff...Added wins and losses links

--- game/src/app/Component/wordentry/wordentry.component.ts
+++ game/src/app/Component/wordentry/wordentry.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {WordentryService} from '../../services/wordentry.service'
+import {gameObj} from '../../../gameObject'
 
 @Component({
     selector: 'wordEntry',
@@ -9,17 +10,22 @@ import {WordentryService} from '../../services/wordentry.service'
 export class WordentryComponent{
     word: string;
     imagePath: string;
-    state: number;
+    dataGame: gameObj;
+    
+
     constructor(private entryword: WordentryService){
-       this.imagePath = "https://image.ibb.co/f0zDVy/State0.png";
-       this.state=0;
+       this.imagePath = "http://adamsouthard.com/myWork/webWork/GIT%20417/SuperheroHangmanWebsite/7_man.png";
+       this.dataGame= new gameObj;
+       
     }
 
     
-
+    
     addTask(event){
         event.preventDefault();
         
+        
+        //filter out what to send to server
         var len=this.word.length;
         if(len==1){
             console.log("You entered a single character!");
@@ -30,11 +36,32 @@ export class WordentryComponent{
         else{
             console.log('invalid entry');
         }
+
+        //receiver the updated dataGame
+        this.dataGame.current= "_ _ _ a _ _"; //simulating an updated dataGame
+        this.dataGame.leng=6;
+        this.dataGame.losses=2;
+        this.dataGame.wins=5;
+        this.dataGame.progress=0;
+        this.dataGame.state=5;
+
+
+        //after you get the object... start here to figure out what to show
         
         //change picture
-        this.state=this.state+1;
+        if(this.dataGame.progress==2){
+            this.updatePic();
+        }
+        else if (this.dataGame.progress==1){ //1 means you won!
+            this.imagePath="https://image.ibb.co/hUuQAy/winner.jpg";
+        }
+        else if(this.dataGame.progress==0){
+            this.imagePath="https://image.ibb.co/dE9mHd/lost.jpg"
+        }
 
-        switch(this.state) {
+    }
+    updatePic(){
+        switch(this.dataGame.state) {
             case 0: this.imagePath="https://image.ibb.co/f0zDVy/State0.png"; break;
             case 1: this.imagePath="https://image.ibb.co/fwxdxd/State1.png"; break;
             case 2: this.imagePath="https://image.ibb.co/cnGriJ/State2.png"; break;
@@ -45,9 +72,8 @@ export class WordentryComponent{
             case 7: this.imagePath="https://image.ibb.co/cGjmiJ/State7.png"; break;
             case 8: this.imagePath="https://image.ibb.co/gc76iJ/State8.png"; break;
             case 9: this.imagePath="https://image.ibb.co/mYuK3J/State9.png"; break;
-            case 10: {this.imagePath="https://image.ibb.co/k9o1Hd/State10.png"; this.state =-1; break;}
-        }
-        
-        
+            case 10: {this.imagePath="https://image.ibb.co/k9o1Hd/State10.png"; this.dataGame.state =-1; break;}
+        }  
+
     }
 }
---
+++ game/src/gameObject.ts
@@ -0,0 +1,17 @@
+export class gameObj{
+    current: String;//Is the _ _ _ _ _ string 
+    state: number;// Nunmber of lives lost...starting with 0
+    leng: number;//length of the word
+    wins: number;// NUmber of wins this player has 
+    losses: number; // number of losses this player has
+    progress: number; //has 3 numbers. 0 => player lost. 1 => player won. 2 => game is still in progress
+    
+    constructor(){
+        this.current = "Let's Play!";
+        this.state = 0;
+        this.leng = 0; //you don't know the word yet
+        this.wins = 0;
+        this.losses =0;
+        this.progress= 2;
+    }
+}
