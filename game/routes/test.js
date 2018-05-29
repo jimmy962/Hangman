dd729376d2553eecd8a21639b4d18ba1568f4d7b
jimmy-ye3 <jhy2116@columbia.edu>
2018-05-29 12:14:56 -0400

    The game works now for single character entry

--- game/routes/test.js
+++ game/routes/test.js
@@ -23,11 +23,11 @@ router.post('/submit',function(req,res,next){
   content=req.body; //Unwrap the object and get the string
   temp= content.stuff;
   isFound=false;
-  if(temp.length==1){ //single character submissions
+  if((temp.length==1)&&(updatedObj.progress==2)){ //single character submissions and when the game is still in progress
       for(i=0;i<updatedObj.leng;i++){
       k=0; //my currentWord string will count by 2 so I will need a seperate index k
       if(temp.charAt(0)==randomWord.charAt(i)){//if I find a match 
-        updatedObj.current=updatedObj.current.substring(0,2*(i))+temp+updatedObj.current.substring(2*(i)+1,2*len-1);
+        updatedObj.current=updatedObj.current.substring(0,2*(i))+temp+updatedObj.current.substring(2*(i)+1,2*updatedObj.leng-1);
         isFound=true;
       }
       k=k+2;
@@ -36,15 +36,32 @@ router.post('/submit',function(req,res,next){
       updatedObj.state++;
     }
   }
+
+  tempString=""; //For checking if I won yet
   
-  if(updatedObj.state==10){
+  if(updatedObj.state>=10){
     updatedObj.progress=0; //signify that the user has lost
     updatedObj.losses++;
   }
+  
+  else{ //User has not lost...check if he has won
+    //Why can't I declare tempString here?
+    length= updatedObj.leng;
+    console.log("length is: "+length);
+    for(i=0;i<length;i++){
+      console.log("yep<"+updatedObj.current.substring(2*i,2*i+1)+">");
+      tempString=tempString+updatedObj.current.substring(2*i,2*i+1);
+      //tempString=tempString + updatedObj.current.substring(2*i,2*i+1);
+    }
+    if(tempString==randomWord){
+      updatedObj.progress=1;
+      updatedObj.losses++;
+    }
+  }
+
   wrapper={stuff: "Got it!", current: updatedObj}
+  console.log("state is: "+updatedObj.state+". Progress is: "+updatedObj.progress);
   res.json(wrapper);
 })
-
-
 
 module.exports = router;
--- game/src/app/Component/wordentry/wordentry.component.ts
+++ game/src/app/Component/wordentry/wordentry.component.ts
@@ -12,20 +12,20 @@ export class WordentryComponent{
     imagePath: string;
     dataGame: gameObj;
     message: string;
+    Meme: string;
     
 
     constructor(private entryword: WordentryService){
-       this.imagePath = "https://image.ibb.co/f0zDVy/State0.png";
+       this.imagePath = "https://image.ibb.co/dG3OYJ/State0.png";
        this.dataGame=new gameObj();
        this.message="Let's play!";
+       this.Meme="https://image.ibb.co/euPafy/you_can_do_it_meme_generator_net_13822620.png"
        this.entryword.getDataObject()
             .subscribe(initData => {
                 console.log(initData);
                 this.dataGame=initData;
             })
-    }
-
-    
+    }  
     
     addTask(event){
         event.preventDefault();
@@ -45,18 +45,15 @@ export class WordentryComponent{
                 this.dataGame=clearEntry.current;
 
                 //which picture to show
-                if(this.dataGame.progress==2){
-                    this.updatePic();
-                }
-                else if (this.dataGame.progress==1){ //1 means you won!
-                    this.imagePath="https://image.ibb.co/hUuQAy/winner.jpg";
+                this.updatePic();
+                
+                if (this.dataGame.progress==1){ //1 means you won!
+                    this.Meme="https://image.ibb.co/hUuQAy/winner.jpg";
                 }
                 else if(this.dataGame.progress==0){
-                    this.imagePath="https://image.ibb.co/dE9mHd/lost.jpg"
-                }          
+                    this.Meme="https://image.ibb.co/dE9mHd/lost.jpg"
+                }         
             });
-        
-                
         }
         else{
             this.message= "Invalid entry. Please enter a single character or a word of length: " +this.dataGame.leng;
@@ -68,18 +65,19 @@ export class WordentryComponent{
     }
     updatePic(){
         switch(this.dataGame.state) {
-            case 0: this.imagePath="https://image.ibb.co/f0zDVy/State0.png"; break;
-            case 1: this.imagePath="https://image.ibb.co/fwxdxd/State1.png"; break;
-            case 2: this.imagePath="https://image.ibb.co/cnGriJ/State2.png"; break;
-            case 3: this.imagePath="https://image.ibb.co/dcZQcd/State3.png"; break;
-            case 4: this.imagePath="https://image.ibb.co/bCvWHd/State4.png"; break;
-            case 5: this.imagePath="https://image.ibb.co/g6f3Vy/State5.png"; break;
-            case 6: this.imagePath="https://image.ibb.co/hzuVAy/State6.png"; break;
-            case 7: this.imagePath="https://image.ibb.co/cGjmiJ/State7.png"; break;
-            case 8: this.imagePath="https://image.ibb.co/gc76iJ/State8.png"; break;
-            case 9: this.imagePath="https://image.ibb.co/mYuK3J/State9.png"; break;
-            case 10: {this.imagePath="https://image.ibb.co/k9o1Hd/State10.png"; this.dataGame.state =-1; break;}
+            case 0: this.imagePath="https://image.ibb.co/dG3OYJ/State0.png"; break;
+            case 1: this.imagePath="https://image.ibb.co/gDkFfy/State1.png"; break;
+            case 2: this.imagePath="https://image.ibb.co/iYBADJ/State2.png"; break;
+            case 3: this.imagePath="https://image.ibb.co/gOn6nd/State3.png"; break;
+            case 4: this.imagePath="https://image.ibb.co/ex2iYJ/state4.png"; break;
+            case 5: this.imagePath="https://image.ibb.co/eyZK7d/State5.png"; break;
+            case 6: this.imagePath="https://image.ibb.co/jsYCSd/State6.png"; break;
+            case 7: this.imagePath="https://image.ibb.co/mgMz7d/State7.png"; break;
+            case 8: this.imagePath="https://image.ibb.co/gxde7d/State8.png"; break;
+            case 9: this.imagePath="https://image.ibb.co/e2eVDJ/State9.png"; break;
+            case 10: {this.imagePath="https://image.ibb.co/hzfsSd/State10.png"; this.dataGame.state =-1; break;}
         }  
 
     }
 }
+
